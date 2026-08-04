763d02aa308e2996613c8c4bdae3ce5d81040ac8
Chris <46389946+ckuttruff@users.noreply.github.com>
2026-08-04 11:37:46 -0700

    Add linting / test run github actions workflow (#25)

--- sabay/tests.py
+++ sabay/tests.py
@@ -1,12 +1,24 @@
+from django.contrib.staticfiles import finders
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from .views import home
 
 
 class HomeViewTests(TestCase):
-    def test_home_page_uses_the_shared_template(self):
+    def test_root_url_resolves_to_home_view(self):
+        match = resolve('/')
+
+        self.assertEqual(match.func, home)
+
+    def test_home_page_renders_successfully(self):
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertTemplateUsed(response, 'base.html')
+        self.assertContains(response, '<h1>Welcome to Sabay</h1>', html=True)
         self.assertContains(response, 'css/simple.min.css')
+
+    def test_simple_css_is_available_to_staticfiles(self):
+        self.assertIsNotNone(finders.find('css/simple.min.css'))
