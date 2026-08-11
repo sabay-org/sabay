259ef323a374bbb7e7c1645d565bc4b91a48978f
Olga Ishimwe <oishimwe@pdx.edu>
2026-08-11 15:36:01 -0700

    consumer updates

--- sabay/settings.py
+++ sabay/settings.py
@@ -71,6 +71,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sabay.wsgi.application'
 
 ASGI_APPLICATION = 'sabay.asgi.application'
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
