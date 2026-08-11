00ffb6a73fd1a712d11ea9ac7f5031e436cd3393
Olga Ishimwe <oishimwe@pdx.edu>
2026-08-11 16:06:35 -0700

    working redis and channels ws chat

--- sabay/settings.py
+++ sabay/settings.py
@@ -75,7 +75,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND':'channels_redis.core.RedisChannelLayer',
         'CONFIG':{
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [{"address": "redis://127.0.0.1:6379", "socket_timeout": 30}],
         },
     },
 }
