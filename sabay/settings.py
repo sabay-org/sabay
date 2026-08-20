19a74c101fcf5dce7949e3d27a0f95024e8981f9
ethan dibble <edibble@pdx.edu>
2026-08-19 20:46:43 -0700

    rename test db

--- sabay/settings.py
+++ sabay/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
         "TEST": {
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
         },
     }
 }
